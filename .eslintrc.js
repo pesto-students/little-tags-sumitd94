e3c7c836a2913f15e94a3cf402ca6ccbdd41ba11
Sumit Dhanania <sumitdhanania94@gmail.com>
2020-12-15 02:18:22 +0530

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+  overrides: [
+    {
+      files: ['*.test.js'],
+      rules: {
+        'no-undef': 'off',
+        'react/jsx-filename-extension': 'off',
+      },
+    },
+  ],
+};
