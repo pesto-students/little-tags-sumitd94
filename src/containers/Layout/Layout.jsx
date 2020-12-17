aaed73d2cf9f62505ec4892c38067c341c9ff054
Sumit Dhanania <sumitdhanania94@gmail.com>
2020-12-18 02:05:45 +0530

    Add login modal

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -4,9 +4,12 @@ import classNames from './Layout.module.css';
 import ToolBar from '../../components/Navigation/ToolBar/ToolBar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import Footer from '../../components/Navigation/Footer/Footer';
+import Modal from '../../components/UI/Modal/Modal';
+import Login from '../../components/Auth/Login/Login';
 
 const Layout = ({ children }) => {
   const [state, setState] = useState({ showSideDrawer: false });
+  const [showModal, setShowModal] = useState(false);
 
   const sideDrawerClosedHandler = () => {
     setState({ showSideDrawer: false });
@@ -16,14 +19,24 @@ const Layout = ({ children }) => {
     setState((prevState) => ({ showSideDrawer: !prevState.showSideDrawer }));
   };
 
+  const toggleModalHandler = () => {
+    setShowModal((prevState) => !prevState);
+  };
+
   return (
     <>
-      <ToolBar drawerToggleClicked={sideDrawerToggleHandler} />
+      <ToolBar
+        drawerToggleClicked={sideDrawerToggleHandler}
+        toggleModalHandler={toggleModalHandler}
+      />
       <SideDrawer
         open={state.showSideDrawer}
         closed={sideDrawerClosedHandler}
       />
       <main className={classNames.content}>
+        <Modal show={showModal} modalClosed={toggleModalHandler}>
+          <Login />
+        </Modal>
         {children}
       </main>
       <Footer />
