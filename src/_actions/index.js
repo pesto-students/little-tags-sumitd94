df25a3530f0082be514f15b0fe4414e104790f51
admin <admin@example.com>
2020-12-19 17:58:33 +0530

    adding product fetch actions

--- src/_actions/index.js
+++ src/_actions/index.js
@@ -1,7 +1,11 @@
+/* eslint import/no-cycle: [0, { maxDepth: 2 }] */
+
 import userActions from './user.actions';
 import alertActions from './alert.actions';
+import productActions from './products.actions';
 
 export {
   userActions,
   alertActions,
+  productActions,
 };
