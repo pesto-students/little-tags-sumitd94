c893f691383c53410dab1e19d029d078cdd3df27
Manohar <hi4manohar@gmail.com>
2020-12-20 12:28:56 +0530

    scrollTop added

--- src/containers/Home/OnDemand/OnDemand.jsx
+++ src/containers/Home/OnDemand/OnDemand.jsx
@@ -7,6 +7,7 @@ import classNames from './OnDemand.module.css';
 
 const OnDemand = (props) => {
   const redirectHandler = (category) => {
+    window.scrollTo(0, 0);
     props.history.push(`/products/${category}`);
   };
 
