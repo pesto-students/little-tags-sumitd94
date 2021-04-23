40e6870b8003e7017d7098b8273087ad95f40ec1
Sumit Dhanania <sumitdhanania94@gmail.com>
2021-04-23 21:19:20 +0530

    Fix the navigation links in the footer

--- src/components/Navigation/Footer/Categories/Categories.jsx
+++ src/components/Navigation/Footer/Categories/Categories.jsx
@@ -7,22 +7,22 @@ const Categories = () => (
     <span className={classNames.heading}>Categories</span>
     <span className={classNames.category}>
       <NavLink to="/products/electronics">
-        Accessories
+        Accessories (225)
       </NavLink>
     </span>
     <span className={classNames.category}>
       <NavLink to="/products/men-clothing">
-        Shirts
+        Shirts (45)
       </NavLink>
     </span>
     <span className={classNames.category}>
-      <NavLink to="/products/pants">
-        Pants
+      <NavLink to="/products/jeans">
+        Pants (75)
       </NavLink>
     </span>
     <span className={classNames.category}>
       <NavLink to="/products/jackets">
-        Jackets
+        Jackets (10)
       </NavLink>
     </span>
   </li>
